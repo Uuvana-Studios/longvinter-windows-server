2c7cfa9faf491d82d06bc50a622e71df885d3d5b
Jordan <jordan.rouco-miguez@epitech.eu>
2022-03-14 12:18:38 +0100

    Fix: getKey failure
    When hexa key is present multiple times in log file it cause function to fail, fixed it by limiting grep to retrieve only the first match.

--- manager.sh
+++ manager.sh
@@ -1,5 +1,5 @@
 function getKey() {
-	hexaKey=$(grep -Eo "0[xX][0-9a-fA-F]{15}+" Longvinter/Saved/Logs/Longvinter.log)
+	hexaKey=$(grep -Eo -m 1 "0[xX][0-9a-fA-F]{15}+" Longvinter/Saved/Logs/Longvinter.log)
 	hexaKey=${hexaKey: 2}
 	echo $(( 16#$hexaKey ))
 }
