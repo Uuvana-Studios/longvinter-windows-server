648a341b383b85ab4377ad84c926e34c6ba61fdd
Arlo <70302832+cherryblossom2020@users.noreply.github.com>
2022-09-20 21:12:45 -0700

    Update StartServer.bat

--- StartServer.bat
+++ StartServer.bat
@@ -1,4 +1,4 @@
 @echo off
-rem This script makes it simple to launch EAC protected game servers, and to get the log output from the server to display in the Command Prompt
+rem This script makes it simple to launch EAC protected game servers, and to get the log output from the server to display in the Command Prompt.
 cd %~dp0
 powershell -ExecutionPolicy Bypass .\StartServer.ps1 %*
