bfef6e718e5a641a248fa63eadf7d523fc1aa7bf
jalsson <nilsson.niklas.97@gmail.com>
2023-05-10 18:13:43 +0300

    Update 1.10b final f

---
+++ MakeSymbolicLinkServer.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /D "%~dp0"
+mklink /D .\Longvinter\SteamMods ..\..\..\workshop\content\1635450
+mklink /D .\Longvinter\Server\Longvinter\SteamMods ..\..\..\..\..\workshop\content\1635450
